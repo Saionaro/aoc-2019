e86c7d3b7b9a91bee332b6ba573c916e335b4743
Saionaro <shibakow@gmail.com>
2019-12-02 12:34:02 +0300

    day 1.2

--- src/1/index.js
+++ src/1/index.js
@@ -1,5 +1,7 @@
 const data = require("./data").split("\n");
 
+// part 1
+
 const sum = data.reduce((sum, mass) => {
   const massInt = parseInt(mass);
 
@@ -7,3 +9,22 @@ const sum = data.reduce((sum, mass) => {
 }, 0);
 
 console.log(sum); //3372695
+
+// part 2
+
+const sum2 = data.reduce((sum, mass) => {
+  const massInt = parseInt(mass);
+
+  let total = Math.floor(massInt / 3) - 2;
+
+  let fuelNeeded = total;
+
+  while (fuelNeeded) {
+    fuelNeeded = Math.max(0, Math.floor(fuelNeeded / 3) - 2);
+    total += fuelNeeded;
+  }
+
+  return sum + total;
+}, 0);
+
+console.log(sum2); // 5056172
