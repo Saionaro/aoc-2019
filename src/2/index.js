fd1a4bbc5cee06af2a10d4ab4462bd4f2329f3bc
Saionaro <shibakow@gmail.com>
2019-12-02 13:14:39 +0300

    day 2.2 - brutforce solution

--- src/2/index.js
+++ src/2/index.js
@@ -2,20 +2,41 @@ const data = require("./data")
   .split(",")
   .map(num => parseInt(num, 10));
 
-// part 1
+const runProgram = (a, b) => {
+  const copy = [...data];
+  copy[1] = a;
+  copy[2] = b;
 
-data[1] = 12;
-data[2] = 2;
+  for (let i = 0; i < copy.length; i += 4) {
+    if (copy[i] === 99) break;
+    if (copy[i] === 1) {
+      copy[copy[i + 3]] = copy[copy[i + 1]] + copy[copy[i + 2]];
+    }
 
-for (let i = 0; i < data.length; i += 4) {
-  if (data[i] === 99) break;
-  if (data[i] === 1) {
-    data[data[i + 3]] = data[data[i + 1]] + data[data[i + 2]];
+    if (copy[i] === 2) {
+      copy[copy[i + 3]] = copy[copy[i + 1]] * copy[copy[i + 2]];
+    }
   }
 
-  if (data[i] === 2) {
-    data[data[i + 3]] = data[data[i + 1]] * data[data[i + 2]];
+  return copy[0];
+};
+
+// part 1
+
+console.log(runProgram(12, 2)); // 6327510
+
+// part 2
+
+let a, b;
+
+for (let i = 0; i < 100; i++) {
+  for (let j = 0; j < 100; j++) {
+    if (runProgram(i, j) === 19690720) {
+      a = i;
+      b = j;
+      break;
+    }
   }
 }
 
-console.log(data[0]);
+console.log(a * 100 + b);
