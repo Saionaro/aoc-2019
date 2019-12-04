c529835b94676498ceee21c115a1588edfebfb42
Saionaro <shibakow@gmail.com>
2019-12-04 10:53:22 +0300

    day 4.2

--- src/4/index.js
+++ src/4/index.js
@@ -19,10 +19,35 @@ const checkNum = num => {
   return hasDouble;
 };
 
-let count = 0;
+// part 2
+
+const checkNum2 = num => {
+  const stringNum = String(num);
+  let prevNum = parseInt(stringNum[0], 10);
+  let digitsMap = {};
+
+  for (let i = 1; i < stringNum.length; i++) {
+    const currentNum = parseInt(stringNum[i], 10);
+
+    if (currentNum < prevNum) return false;
+    if (currentNum === prevNum) {
+      if (!digitsMap[currentNum]) digitsMap[currentNum] = 1;
+      digitsMap[currentNum]++;
+    }
+
+    prevNum = currentNum;
+  }
+
+  return Object.values(digitsMap).some(count => count === 2);
+};
+
+let count1 = 0;
+let count2 = 0;
 
 for (let i = range[0]; i <= range[1]; i++) {
-  if (checkNum(i)) count++;
+  if (checkNum(i)) count1++;
+  if (checkNum2(i)) count2++;
 }
 
-console.log(count); // 1767
+console.log(count1); // 1767
+console.log(count2); // 1192
