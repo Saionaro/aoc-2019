6af2c43dbc15d2e81008a99afd95f2bef96bc654
Saionaro <shibakow@gmail.com>
2019-12-16 13:32:18 +0300

    day 16.2

--- src/16/index.js
+++ src/16/index.js
@@ -1,6 +1,4 @@
-const data = require("./data")
-  .split("")
-  .map(item => parseInt(item, 10));
+const rawData = require("./data");
 
 const BASE_PATTERN = [0, 1, 0, -1];
 
@@ -26,10 +24,13 @@ const iterate = input => {
   for (let i = 1; i <= input.length; i++) {
     const gen = getPattern(i);
 
-    const sum = input.reduce((acc, item) => {
-      return acc + gen.next().value * item;
-    }, 0);
+    let sum = 0;
 
+    for (let i = 0; i < input.length; i++) {
+      sum += gen.next().value * input[i];
+    }
+
+    gen.next(true);
     acc.push(Math.abs(sum % 10));
   }
 
@@ -38,10 +39,48 @@ const iterate = input => {
 
 // part 1
 
-let input = data;
+let input = rawData.split("").map(item => parseInt(item, 10));
 
-for (let i = 0; i < 100; i++) {
-  input = iterate(input);
-}
+const solve1 = (offset, inputSignal) => {
+  let signal = inputSignal;
 
-console.log(input.slice(0, 8).join("")); // 59522422
+  for (let i = 0; i < 100; i++) {
+    signal = iterate(signal);
+  }
+
+  return signal.slice(offset, offset + 8).join("");
+};
+
+console.log(solve1(0, input)); // 59522422
+
+// part 2
+
+let input2 = rawData
+  .repeat(10000)
+  .split("")
+  .map(item => parseInt(item, 10));
+
+const solve2 = (offset, inputSignal) => {
+  let signal = inputSignal;
+
+  for (let i = 0; i < 100; i++) {
+    const newSignal = [];
+
+    for (let j = signal.length - 1; j >= 0; j--) {
+      if (j < (3 * signal.length) / 4) {
+        newSignal.push(0);
+      } else if (j == signal.length - 1) {
+        newSignal.push(signal[signal.length - 1]);
+      } else {
+        const next = Math.abs(newSignal[newSignal.length - 1] + signal[j]) % 10;
+        newSignal.push(next);
+      }
+    }
+
+    signal = newSignal.reverse();
+  }
+
+  return signal.slice(offset, offset + 8).join("");
+};
+
+console.log(solve2(parseInt(rawData.slice(0, 7), 10), input2)); // 18650834
